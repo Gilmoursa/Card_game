a6a077f32abaf74e03852dc64a0a5665bb62bbb2
Gilmoursa <gilmoursa@gmail.com>
2015-10-09 10:05:25 -0400

    simple CLI

--- app/models/gamelogic_cli.rb
+++ app/models/gamelogic_cli.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class Gamelogic_cli
-	attr_accessor :input
 	# attr_reader :play_deck
+	attr_reader :user_input
+	attr_reader :player1, :player2
 
 	def initialize
 		puts "Welcome to War!"
+		puts "Type help to learn about the game or play to start playing"
 		@play_deck = Deck.new
 		@play_deck.deal
 		self.assign_players
@@ -11,31 +14,39 @@ class Gamelogic_cli
 	end
 
 	def assign_players
-		player1 = Player.new(@play_deck.hand_a)
-		player2 = Player.new(@play_deck.hand_b)
+		@player1 = Player.new(@play_deck.hand_a)
+		@player2 = Player.new(@play_deck.hand_b)
 	end
 
 	def help
 		puts "Each player turns up a card at the same time and the player with the higher card takes both cards and puts them, face down, on the bottom of his stack. If the cards are the same rank, it is War. Each player turns up one card face down and one card face up. The player with the higher cards takes both piles (six cards)."
 	end
 
+	def input
+		@user_input=gets.downcase.chomp
+	end
+
 	def cli 
-		while input != 'exit'
-			case input
+		while @user_input != 'exit'
+			input
+			case @user_input
 			when 'help'
 				help
 			when 'play'
 				play
+			when 'exit'
+				break
 			else
-				puts "I didn't understand your command"
+				puts "I didn't understand your command, type help to get help or play to start playing."
 			end
 		end
 		puts "Goodbye, thanks for playing!"
 	end
 
 	def play
-		puts "Player 1 played #{player1.card_escrow}"
-		puts "Player 2 played #{player2.card_escrow}"
+		#binding.pry
+		puts "Player 1 played #{@player1.play_hand}"
+		puts "Player 2 played #{@player2.play_hand}"
 		puts "#{self.is_greater?} wins"
 	end
 
@@ -44,12 +55,12 @@ class Gamelogic_cli
 	end
 
 	def is_greater?
-		if player1.card_escrow[0...-1].to_i > player2.card_escrow[0...-1].to_i
-		 	player1
+		if @player1.card_escrow[0...-1].to_i > @player2.card_escrow[0...-1].to_i
+		 	"player 1"
 		# elsif player1.card_escrow < player2.card_escrow
 		# 	player2
 		else
-			player2 
+			"player 2" 
 		end
 	end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,12 @@
 class Player
 	attr_reader :hand, :card
+	attr_accessor :card_escrow
 
 	def initialize(hand)
 		@hand = hand
 	end
 
 	def play_hand
-		card_escrow = self.hand.shift
+		self.card_escrow = self.hand.shift
 	end
 end
